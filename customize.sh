9567266f21d013040e69da63e08a10d6bce16574
SunvidWong <45497358+SunvidWong@users.noreply.github.com>
2021-01-29 00:48:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Author: Sunvid
+# Sunvid Channel: https://b.sunvid.cn/
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.6.1/g' openwrt/package/base-files/files/bin/config_generate
